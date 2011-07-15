be298ecf865fc50ef058cb1b39e3e5c19cae6c18
Mitch Crowe <mitch.crowe@suite101.com>
2011-07-15 13:54:40 -0700

    Creation of initial rails template.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Louvre::Application.config.secret_token = 'f6914cba558cd548975e4770e5f048407b15d24a370e6eddba17f06207d3f5632e430cff883e1f8d0fdc659690f604f31a8a3b47f89ddbc277d4adb60c452ee8'
