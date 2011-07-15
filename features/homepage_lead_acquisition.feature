33a10b5572a0ce0469c6e16a7a6e2731b1f51726
Mitch Crowe <mitch.crowe@suite101.com>
2011-07-15 14:28:15 -0700

    Create an empty homepage.

---
+++ features/homepage_lead_acquisition.feature
@@ -0,0 +1,11 @@
+Feature: homepage_lead_acquisition
+    In order to create a canvas
+    People should be able to
+    Get to an email signup page
+
+Scenario:
+    Given I am on the homepage
+    When I fill in the following:
+        | email   | gill.fert@foaca.org |
+        | password | foaca |
+    Then I should be taken to the "Thanks for your interest" page
---
+++ features/step_definitions/homepage_lead_acquisition_steps.rb
@@ -0,0 +1,11 @@
+Given /^that I am on the homepage$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I click on the "([^"]*)" link$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should be taken to an email form$/ do
+  pending # express the regexp above with the code you wish you had
+end
