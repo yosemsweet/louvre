33436481918439252acb3ed9c36cc2ed5925890e
Yosem Sweet <yosem.sweet@suite101.com>
2011-07-19 11:28:09 -0700

    [#15781735] Cleaning up some specs and cuke features

--- features/support/current_user.rb
+++ features/support/current_user.rb
@@ -2,6 +2,10 @@ module KnowsCurrentUser
 	def current_user
 		@current_user ||= Factory.create(:user) 
 	end
+	
+	def set_current_user(user)
+		@current_user = user
+	end
 end
 
 World(KnowsCurrentUser)
