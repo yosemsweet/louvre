04446dad59af481bcd45ac8d81a97c0a192cd9f3
Yosem Sweet <yosem.sweet@suite101.com>
2011-07-19 10:24:20 -0700

    [#15781735] Added owners to canvae

--- app/models/canvas.rb
+++ app/models/canvas.rb
@@ -1,4 +1,7 @@
 class Canvas < ActiveRecord::Base
+	belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
+
 	validates :name, :presence => true, :uniqueness => true
 	validates :mission, :presence => true
+	validates :owner, :presence => true
 end
--- spec/models/canvas_spec.rb
+++ spec/models/canvas_spec.rb
@@ -7,25 +7,39 @@ describe Canvas do
 			canvas.should be_valid
 		end
 	
-		context "Name" do
-			it "should be required" do
-		  	canvas = Factory.build(:canvas, :name => "")
-		    canvas.should_not be_valid
-		  end
+		context "attribute validations" do
+			context "Name" do
+				it "should be required" do
+			  	canvas = Factory.build(:canvas, :name => "")
+			    canvas.should_not be_valid
+			  end
 		
-			it "should be unique" do
-				canvas = Factory.build(:canvas, :name =>"foo")
-				canvas.should be_valid
-				canvas.save #uniqueness only tests against saved values - is this appropriate?
-				canvas_duplicate_name = Factory.build(:canvas, :name =>"foo")
-				canvas_duplicate_name.should_not be_valid
+				it "should be unique" do
+					canvas = Factory.build(:canvas, :name =>"foo")
+					canvas.should be_valid
+					canvas.save #uniqueness only tests against saved values - is this appropriate?
+					canvas_duplicate_name = Factory.build(:canvas, :name =>"foo")
+					canvas_duplicate_name.should_not be_valid
+				end
 			end
-		end
 		
-		context "Mission" do
-			it "should be required" do
-				canvas = Factory.build(:canvas, :mission => "")
-				canvas.should_not be_valid
+			context "Mission" do
+				it "should be required" do
+					canvas = Factory.build(:canvas, :mission => "")
+					canvas.should_not be_valid
+				end
+			end
+			
+			context "Owner" do
+				it "should have an owner" do
+					canvas = Factory.build(:canvas)
+					canvas.should respond_to(:owner)
+				end
+				
+				it "should be required" do
+					canvas = Factory.build(:canvas, :owner => nil)
+					canvas.should_not be_valid
+				end
 			end
 		end
 	
