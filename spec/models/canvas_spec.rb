1be80d7a8ae348ce77f6d2a2d2193630c300fd2e
Yosem Sweet <yosem.sweet@suite101.com>
2011-07-19 09:57:33 -0700

    [#15781735] Added uniqueness and presence of name tests for canvae

--- spec/models/canvas_spec.rb
+++ spec/models/canvas_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Canvas do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Validations" do
+	
+		context "Canvas Name" do
+			it "should be required" do
+		  	canvas = Factory.build(:canvas, :name => "")
+		    canvas.should_not be_valid
+		  end
+		
+			it "should be unique" do
+				canvas = Factory.build(:canvas, :name =>"foo")
+				canvas.should be_valid
+				canvas.save #uniqueness only tests against saved values - is this appropriate?
+				canvas_duplicate_name = Factory.build(:canvas, :name =>"foo")
+				canvas_duplicate_name.should_not be_valid
+			end
+		end
+	
+	end
 end
