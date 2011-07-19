7f951aef98d3991d18b553e9fd3bddfb9738656a
Yosem Sweet <yosem.sweet@suite101.com>
2011-07-18 17:45:35 -0700

    [#15781735] Start create canvas wizard scenario works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110718173905) do
+ActiveRecord::Schema.define(:version => 20110719003356) do
+
+  create_table "canvae", :force => true do |t|
+    t.string   "name"
+    t.text     "mission"
+    t.string   "image_url"
+    t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "provider"
