80958b94ee454139b2b26ee6d5fc31c0914a8426
Chris Reid <chris.reid@suite101.com>
2011-07-20 14:30:18 -0700

    fixes syntax error in migration

--- db/migrate/20110719233216_change_creator_to_creator_id_for_pages.rb
+++ db/migrate/20110719233216_change_creator_to_creator_id_for_pages.rb
@@ -1,7 +1,7 @@
 class ChangeCreatorToCreatorIdForPages < ActiveRecord::Migration
 	
   def self.up
-    remove_column :pages, :creator, :integer
+    remove_column :pages, :creator
 	  add_column :pages, :creator_id, :integer
   end
 
