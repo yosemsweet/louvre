afab58cd5f1b7618495012af912577b0d6b12174
Rafe Hatfield <rafe.hatfield@suite101.com>
2011-07-20 12:00:48 -0700

    created cucumber tests for a list of pages on a canvas [#15784209]

--- spec/factories/canvae.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-Factory.define :canvas do |f|
-  f.name "Fashion of a Certain Age"
-  f.mission "Better clothing for women of a certain age!"
-  f.image_url "http://www.carniola.org/theglory/images/McHammer.gif"
-	f.association :creator, :factory => :user
-end
