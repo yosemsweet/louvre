9cbb48467c5e59af9c9f6e2a627adc8ab03416f1
jennifer <jennifer.chui@suite101.com>
2011-07-21 13:53:24 -0700

    [15781447] Added a check to make sure valid params return a 201 with comment create.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -15,6 +15,15 @@ describe CommentsController do
             :thing_id => comment.thing.id
           assigns(:comment).content.should == comment.content and assigns(:comment).creator.should == comment.creator and assigns(:comment).thing.should == comment.thing
         end
+        
+        it "returns a success code" do
+          results = post :create, 
+            :comment => {:content => comment.content, :creator_id => comment.creator.id}, 
+            :format => 'json', 
+            :canvas_id => comment.thing.canvas.id, 
+            :thing_id => comment.thing.id
+          results.status.should == 201
+        end
       end
       
       describe "without valid params" do
