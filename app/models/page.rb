2e21b2aeefa0e8f47bd37c98fcf00dbd2543c6a6
Mitch Crowe <mitch.crowe@suite101.com>
2011-07-22 17:31:31 -0700

    Add revisions method to page.

--- app/models/page.rb
+++ app/models/page.rb
@@ -8,4 +8,24 @@ class Page < ActiveRecord::Base
 	validates_presence_of :creator
 	validates_presence_of :canvas
 	
+	def revisions_list
+	  page_versions = []
+	  self.widgets.each do |w|
+	    w.versions.each do |v|
+	     page_versions << v
+      end
+    end
+    
+    page_versions = page_versions.sort {|pv| pv.created_at.to_date}
+    
+    revisions = ""
+    
+    page_versions.each do |pv|
+      revisions << "#{pv.reify.content}|"
+    end
+    
+    revisions
+        
+  end
+	
 end
--- app/models/widget.rb
+++ app/models/widget.rb
@@ -3,6 +3,8 @@ class Widget < ActiveRecord::Base
   belongs_to :page
   belongs_to :canvas
 	belongs_to :creator, :class_name => "User"
+
+  has_paper_trail
     
   validates_presence_of :canvas
   validates_presence_of :creator
