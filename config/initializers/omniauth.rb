109e0673da82e642c0ef09b367545d67d5a64c28
Chris Reid <chris.reid@suite101.com>
2011-07-25 13:12:52 -0700

    adds support for seperate dev / test fb apps

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
-Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :facebook, '178863089564', '5df447b8480c97b38307cae54e1627c0' , :scope => "email"
-  provider :twitter, 'FSTx5kL5Ni9bl6sClZf5Pw', '0BWf0FCBXpgduFevvOhbjMrvMsTbRk4AUpMnhMlqU'  
+Rails.application.config.middleware.use OmniAuth::Builder do
+  if Rails.env.production?
+    provider :facebook, '141901352559841', '8f146b3b2120f3d416148eecedce0e27' , :scope => "email"
+    provider :twitter, 'FSTx5kL5Ni9bl6sClZf5Pw', '0BWf0FCBXpgduFevvOhbjMrvMsTbRk4AUpMnhMlqU'
+  elsif Rails.env.development? || Rails.env.test? 
+    provider :facebook, '196297497094752', 'f2b1ddb79af75dbd498752f37fddc013' , :scope => "email"  
+  end
 end
