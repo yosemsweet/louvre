e584cb351e64523e25d4561d63b2ffc172d52cd3
Rafe Hatfield <rafe.hatfield@suite101.com>
2011-08-03 14:51:59 -0700

    removed development routes and files; added scrolly to home page; amalgamated css files [#16384885]

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,5 @@ Louvre::Application.routes.draw do
 	match "/logout" => "sessions#destroy", :as => :logout 
 	
 	match "/next_scroll_widgets" => "widgets#next_for_scroll"
-	match "/scrolly" => "static#scrolly"
-	match "/scrolly2" => "static#scrolly2"
 	match "/scrollywidgets" => "static#scrolly_widgets"
 end
