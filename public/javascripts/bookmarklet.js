64a7a3deb9c374ed675796f03edd218b87125c87
Mitch Crowe <mitch.crowe@suite101.com>
2011-08-05 18:24:33 -0700

    Make styling of bookmarklet dialog more consistent across pages.

--- public/javascripts/bookmarklet.js
+++
@@ -1,7 +0,0 @@
-javascript:(function(){_my_script=document.createElement('SCRIPT');_my_script.type='text/javascript';_my_script.src='http://localhost:3000/javascripts/post-link.js';
-
-_my_script.innerElement='var%20canvas_id=';
-
-
-
-element=document.getElementsByTagName('body')[0];element.appendChild(_my_script);})();
