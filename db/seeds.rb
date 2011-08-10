2999b08f79c3966fe8f87e393e9b6c35c2cb015a
Mitch Crowe <mitch.crowe@suite101.com>
2011-08-10 14:09:43 -0700

    Add some seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create(
+  :name => "Bob Dole",
+  :provider => "facebook",
+  :uid => 21009266,
+  :image => "https://s3.amazonaws.com/randomfile/1.png",
+  :email => "bob.dole@gmail.com"
+)
+
+1.upto(10) do |i|
+  Canvas.create(
+    :name => "Canvas number #{i}",
+    :mission => "The mission that is the mission for canvas #{i}",
+    :image => "https://s3.amazonaws.com/randomfile/#{i%5 + 1}.png"
+  )
+end
