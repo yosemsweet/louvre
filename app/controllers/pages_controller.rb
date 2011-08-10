433542ee89e2e308eb29dabbf890b545564d2460
Rafe Hatfield <rafe.hatfield@suite101.com>
2011-08-10 10:21:47 -0700

    moved comment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
 	before_filter :require_login, :except => [:show, :index, :widgets]
   before_filter :only => [:show, :edit, :versions, :widgets] do
     @page = Page.find(params[:id])
-    @title = @page.canvas.name
 		# for header in application layout
+    @title = @page.canvas.name
     @canvas = @page.canvas
   end
 
