9b0db7c260dcde4ed8f03d6f4b2fd952dda901ee
jennifer <jennifer.chui@suite101.com>
2011-08-11 12:26:46 -0700

    [#16822845] sliding canvae viewer

--- db/schema.rb
+++ db/schema.rb
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(:version => 20110804000341) do
     t.datetime "updated_at"
   end
 
+  create_table "invites", :force => true do |t|
+    t.integer  "inviter_id"
+    t.string   "invitee_email"
+    t.integer  "canvas_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "pages", :force => true do |t|
     t.string   "title"
     t.datetime "created_at"
@@ -82,8 +90,8 @@ ActiveRecord::Schema.define(:version => 20110804000341) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "content"
-    t.integer  "parent_id"
     t.string   "alt_text"
+    t.integer  "parent_id"
   end
 
 end
