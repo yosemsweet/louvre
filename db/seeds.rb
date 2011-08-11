342cc12e34de397df2ae2899ac51046cc1a3b08e
Mitch Crowe <mitch.crowe@suite101.com>
2011-08-11 12:11:11 -0700

    List tags on widgets.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-User.create(
-  :name => "Bob Dole",
-  :provider => "facebook",
-  :uid => 21009266,
-  :image => "https://s3.amazonaws.com/randomfile/1.png",
-  :email => "bob.dole@gmail.com"
-)
-
-1.upto(10) do |i|
-  Canvas.create(
-    :name => "Canvas number #{i}",
-    :mission => "The mission that is the mission for canvas #{i}",
-    :image => "https://s3.amazonaws.com/randomfile/#{i%5 + 1}.png"
-  )
-end
+# User.create(
+#   :name => "Bob Dole",
+#   :provider => "facebook",
+#   :uid => 21009266,
+#   :image => "https://s3.amazonaws.com/randomfile/1.png",
+#   :email => "bob.dole@gmail.com"
+# )
+# 
+# 1.upto(10) do |i|
+#   Canvas.create(
+#     :name => "Canvas number #{i}",
+#     :mission => "The mission that is the mission for canvas #{i}",
+#     :image => "https://s3.amazonaws.com/randomfile/#{i%5 + 1}.png"
+#   )
+# end
 
 filename = 'db/tags.txt'
 file = File.open(filename, 'r')
