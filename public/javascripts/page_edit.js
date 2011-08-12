4eb36207934fe8d9176e5ccb4b1157a4ec46a57a
Mitch Crowe <mitch.crowe@suite101.com>
2011-08-12 15:18:17 -0700

    Fix sortable list for page edit.

--- public/javascripts/page_edit.js
+++ public/javascripts/page_edit.js
@@ -26,7 +26,7 @@ $(document).ready( function(){
 	      opacity: 0.6,
 	      revert: 'invalid',
 	      revertDuration: 200,
-	      handle: 'img.handle'
+	      handle: '.widget'
 	    });
     
 	    enable_widget_previews();
@@ -49,7 +49,7 @@ $(document).ready( function(){
 	   tolerance: 'pointer',
 	   placeholder: 'placeholder',
 	   forcePlaceholderSize: true,
-	   handle: 'img.handle',
+	   handle: false,
 	   update: function(event, ui){     
 	      // Get the dragged widget id.
 	      var widget_id = ui.item.data("widget_id");
