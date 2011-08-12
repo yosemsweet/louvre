569c33a18f29e9151f967f5c8a206942f4fc094b
Mitch Crowe <mitch.crowe@suite101.com>
2011-08-12 08:47:49 -0700

    Clean up adding widget javascript.

--- public/javascripts/widget.js
+++ public/javascripts/widget.js
@@ -5,25 +5,37 @@ $(function() {
 	  minWidth:500, 
 	  autoOpen:false
 	});
+	$widget_dialog.open = function(title, src){
+		$("iframe", $(this)).attr("src", src);
+		$(this).dialog("option","title", title);
+		$(this).dialog("open");
+	}
+	$widget_dialog.close = function(){
+		$(this).dialog("close");
+	}
     
 	$(".add_widget").click(function(event){
 	  event.preventDefault();
-	  if(request.page_id === 0){
-		  var page_query = '';
-	  } else {
+	
+		// Canvas id and content type are required.
+		var canvas_id = $(this).data("canvas_id");
+		var content_type = $(this).data("content_type");
+		
+		// If page id is defined, we attach the widget to that page.
+		var page_query = '';
+		var page_id = $(this).data("page_id");	
+	  if(typeof page_id !== "undefined"){
 		  var page_query = '&page_id=' + request.page_id;
 	  }
-	  $("iframe", $widget_dialog).attr("src", "/widgets/new?content_type=" + $(this).data("content_type") + "&canvas_id=" + request.canvas_id + page_query);
-	  $widget_dialog.dialog("option","title", "New Widget");
-	  $widget_dialog.dialog("open");
+	
+		$widget_dialog.open("New Widget", "/widgets/new?content_type=" + content_type + "&canvas_id=" + canvas_id + page_query);
 	});
 
 	$(".widget .edit").live("click", function(event){
 	  var widget_id = $(this).parents(".widget").data("widget_id");
-	  $("iframe", $widget_dialog).attr("src", "/widgets/" + widget_id + "/edit");
-	  $widget_dialog.dialog("option","title", "Edit Widget");
-	  $widget_dialog.dialog("open");
   
+		$widget_dialog.open("Edit Widget", "/widgets/" + widget_id + "/edit")
+
 	  mixpanel_attributes.widget_id = widget_id;
 	  mpq.push(["track","canvas_edit_widget", mixpanel_attributes]);
   
@@ -31,7 +43,7 @@ $(function() {
 	});
 
   close_widget_dialog = function(){
-    $widget_dialog.dialog("close");
+    $widget_dialog.close();
   }
 
 });
