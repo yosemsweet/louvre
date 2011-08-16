591d6a9602bc8d8121b03eca6764f2bdf73a5b33
Yosem Sweet <yosem.sweet@suite101.com>
2011-08-16 15:16:42 -0700

    Trying to get bookmarklet helper working

--- features/bookmarklet.feature
+++ features/bookmarklet.feature
@@ -3,11 +3,12 @@ As Gill Fert
 I want to add content to my canvas input stream via bookmarklets 
 so that I can share interesting information with my friends
 
-@background_user
+@background_user, @wip
 Scenario: Use bookmarklet from a web page adds to input stream
 Given I am browsing "cnn"
 And I have a canvas bookmarklet
 When I use the bookmarklet
+Then I see an "Add Link" dialog
 Then the webpage link is added to the canvas' input stream
 
 @background_user
--- features/step_definitions/input_stream_steps.rb
+++ features/step_definitions/input_stream_steps.rb
@@ -1,22 +1,7 @@
-Given /^I am browsing "([^"]*)"$/ do |page|
-  set_current_page(page)
-end
+
 
 Then /^I should see the image "(.+)"$/ do |image|
     page.should have_xpath("//img[\"imageurl\"]")
-end
-
-Given /^I have a canvas bookmarklet$/ do
-  Given "there is a canvas"
-end
-
-Given /^I have selected some text on the screen$/ do
-	set_selection("<h2>CNN</h2>")
-end
-
-When /^I use the bookmarklet$/ do
-  result = post "/widgets/", :canvas_id => Canvas.last.id, :widget => {:link => current_page, :title => "CNN", :creator_id => current_user.id, :content_type => 'link_content', :text => selection.html_safe}
-  set_input_stream_call_status(result.status)
 end
 
 Then /^the webpage link is added to the canvas' input stream$/ do
