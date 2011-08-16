367a619838c2e876d3fd4cba2d396c1545ad21da
Yosem Sweet <yosem.sweet@suite101.com>
2011-08-16 15:21:48 -0700

    Got bookmarklet helper working

--- features/step_definitions/canvas_homepage_steps.rb
+++ features/step_definitions/canvas_homepage_steps.rb
@@ -1,9 +1,8 @@
 Then /^I can install the canvas bookmarklet$/ do
   bookmarklet = "javascript:(function(){_my_var=document.createElement('SCRIPT');_my_var.type='text/javascript';_my_var.innerText='canvas_id=#{Canvas.last.id};user_id=#{current_user.id}';_my_script=document.createElement('SCRIPT');_my_script.type='text/javascript';_my_script.src='#{host_uri}/javascripts/post-link.js';element=document.getElementsByTagName('body')[0];element.appendChild(_my_var);element.appendChild(_my_script);})();"
   
-	puts page.find("a#bookmarklet")[:href]
-
-	page.should have_selector( "a#bookmarklet[href='#{bookmarklet}]") do |link|
+	page.should have_selector( "a#bookmarklet") do |link|
+		link[:href].should == bookmarklet
     link.should have_content("Add to #{Canvas.last.name}")
   end
 end
