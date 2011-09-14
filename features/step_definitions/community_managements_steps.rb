86e69702e459d4180cdcaff5dfce08fd4153f097
Yosem Sweet <yosem.sweet@suite101.com>
2011-09-14 13:03:46 -0700

    Fixing a merge problem

--- features/step_definitions/community_managements_steps.rb
+++ features/step_definitions/community_managements_steps.rb
@@ -6,6 +6,10 @@ Given /^"([^"]*)" is a user$/ do |user|
 	Factory.create(:user, :name => user) unless User.exists?(:name => user)
 end
 
+Given /^I am a member of that canvas$/ do
+	current_user.set_canvas_role(Canvas.last, :member)
+end
+
 Given /^I have banned "([^"]*)"$/ do |user|
   User.where(:name => user).first.set_canvas_role(Canvas.last, :banned)
 end
