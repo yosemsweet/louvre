1dd1be2ebce30b92b99b5816507f6ea852276560
Aaron Park <aaron.park@suite101.com>
2011-09-20 08:55:08 -0700

    Events now diusplayed in the header

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,13 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.where(:canvas_id => current_user.canvas_ids).order(:created_at).reverse_order.limit(5)
+    u = current_user
+    u.notifications_viewed_at = Time.now
+    u.save
     render :layout => false
   end
   
   def count
-    render :json => Event.count()
+    render :json => Event.where("canvas_id = ? AND created_at >= ?",  current_user.canvas_ids, current_user.notifications_viewed_at).count()
   end
 end
