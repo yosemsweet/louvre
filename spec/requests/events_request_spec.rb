42fb69f860adb327496bc7fe3201885e7f53334b
Aaron Park <aaron.park@suite101.com>
2011-09-22 11:53:40 -0700

    cleaning up the tests to support postgres.

--- spec/requests/events_request_spec.rb
+++ spec/requests/events_request_spec.rb
@@ -4,6 +4,7 @@ describe "Pages Requests" do
   
   before(:each) do
     @user = Factory.create(:user)
+    @canvas = Factory.create(:canvas)
   end
 
   describe "GET /event_count" do    
@@ -16,6 +17,7 @@ describe "Pages Requests" do
      it "should return 200 for a logged in person" do
        # Stub the current_user method so it appears like a user is logged in.
        EventsController.any_instance.stubs(:current_user).returns(@user)
+       @user.set_canvas_role(@canvas, :member)
        get event_count_path
        response.status.should == 200
      end
@@ -23,6 +25,7 @@ describe "Pages Requests" do
      it "should return 0 as the event count for a logged in person with no events" do
        # Stub the current_user method so it appears like a user is logged in.
         EventsController.any_instance.stubs(:current_user).returns(@user)
+        @user.set_canvas_role(@canvas, :member)
         get event_count_path
         response.body.should == "0"
       end
@@ -38,6 +41,7 @@ describe "Pages Requests" do
 
       it "should return 200 for a logged in person" do
         EventsController.any_instance.stubs(:current_user).returns(@user)
+        @user.set_canvas_role(@canvas, :member)
         get events_path
         response.status.should == 200
       end
