4c3437db1dfbb77c7bc7be760259f6e3c4001beb
Rafe Hatfield <rafe.hatfield@suite101.com>
2011-09-26 10:48:39 -0700

    finished creating basic page controller spec, without editor  [#18716337]

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -60,10 +60,6 @@ describe PagesController do
 	
     describe "failure" do
       before(:each) do
-		    @canvas = Factory(:canvas)
-		    @user = Factory.create(:user)
-		    PagesController.any_instance.stubs(:current_user).returns(@user)
-				@user.set_canvas_role(@canvas,:member)
         @attr = { :title => "" }
       end
 
@@ -96,7 +92,97 @@ describe PagesController do
       #   response.should redirect_to(canvas_page_path(assigns(:page)))
       # end 
     end
-
 	end
+
+  describe "GET 'edit'" do
+    before(:each) do
+	    @page = Factory(:page)
+	    @user = Factory.create(:user)
+	    PagesController.any_instance.stubs(:current_user).returns(@user)
+			@user.set_canvas_role(@page.canvas,:member)
+    end
+
+    it "should be successful" do
+      get :edit, :canvas_id => @page.canvas_id, :id => @page
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get :edit, :canvas_id => @page.canvas_id, :id => @page
+      response.body.should have_selector("title", :content => @page.canvas.name)
+    end
+
+    it "should have a page title field" do
+      get :edit, :canvas_id => @page.canvas_id, :id => @page
+      response.body.should have_selector('input[id="page_title"][name="page[title]"]')
+    end
+  end
+
+  describe "PUT 'update'" do
+
+    before(:each) do
+      @page = Factory(:page)
+	    @user = Factory.create(:user)
+	    PagesController.any_instance.stubs(:current_user).returns(@user)
+			@user.set_canvas_role(@page.canvas,:member)
+    end
+
+    # describe "failure" do
+    # 
+    #   before(:each) do
+    #     @attr = { :title => "" }
+    #   end
+    # 
+    #   it "should render the 'edit' page" do
+    #     put :update, :canvas_id => @page.canvas_id, :id => @page, :page => @attr
+    # 				puts response.body
+    #     response.should render_template('edit')
+    #   end
+    # 
+    #   it "should have the right title" do
+    #     put :update, :canvas_id => @page.canvas_id, :id => @page, :page => @attr
+    #     response.body.should have_selector("title", :content => @page.canvas.name)
+    #   end
+    # 
+    # end
+
+    describe "success" do
+      before(:each) do
+        @attr = { :title => "New Title" }
+      end
+    
+      it "should change the user's attributes" do
+        put :update, :canvas_id => @page.canvas_id, :id => @page, :page => @attr
+        @page.reload
+        @page.title.should  == @attr[:title]
+      end
+    
+      it "should redirect to the page show page" do
+        put :update, :canvas_id => @page.canvas_id, :id => @page, :page => @attr
+        response.should redirect_to(canvas_page_path(@page.canvas, @page))
+      end
+    end
+
+  end
+
+  describe "DELETE 'destroy'" do
+    before(:each) do
+      @page = Factory(:page)
+	    @user = Factory.create(:user)
+	    PagesController.any_instance.stubs(:current_user).returns(@user)
+			@user.set_canvas_role(@page.canvas,:member)
+    end
+
+    it "should destroy the page" do
+      lambda do
+        delete :destroy, :canvas_id => @page.canvas, :id => @page
+      end.should change(Page, :count).by(-1)
+    end
+
+    it "should redirect to the canvas page" do
+      delete :destroy, :canvas_id => @page.canvas, :id => @page
+      response.should redirect_to(canvas_path(@page.canvas))
+    end  
+  end
 
 end
