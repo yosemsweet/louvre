1fa4f51f0266aa64950cda2e5dc547469ba08cbd
Aaron Park <aaron.park@suite101.com>
2011-09-26 09:48:29 -0700

    added editor migration to canvas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110926153448) do
+ActiveRecord::Schema.define(:version => 20110926164106) do
 
   create_table "canvae", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20110926153448) do
     t.datetime "updated_at"
     t.integer  "creator_id"
     t.boolean  "open",       :default => true
+    t.integer  "editor_id"
   end
 
   create_table "canvas_applicants", :force => true do |t|
