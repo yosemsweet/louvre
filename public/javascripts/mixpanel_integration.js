a8116e849d7e6e8880063bb70942105c0d69fa8f
Yosem Sweet <yosem.sweet@suite101.com>
2011-09-29 10:47:29 -0700

    [fixes #19029371] Set up mixpanel to use separate dev, test, and live accounts

--- public/javascripts/mixpanel_integration.js
+++ public/javascripts/mixpanel_integration.js
@@ -1,5 +1,5 @@
 var mpq = [];
-mpq.push(["init", "514bbd502c105398458bf10f8b686b0d"]);
+mpq.push(["init", request.mixpanel_account]);
 (function() {
  var mp = document.createElement("script"); mp.type = "text/javascript"; mp.async = true;
  mp.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + "//api.mixpanel.com/site_media/js/api/mixpanel.js";
