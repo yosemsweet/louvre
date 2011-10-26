4fc6b78089f95ae7816c08f5b8719c1b56d735ab
Rafe Hatfield <rafe.hatfield@suite101.com>
2011-10-26 15:20:33 -0700

    changed widget observer so event now records page or canvas that is required for link, rather than the widget itself which stops issues when widget is then deleted [#20205513]

--- app/models/widget_observer.rb
+++ app/models/widget_observer.rb
@@ -4,21 +4,24 @@ class WidgetObserver < ActiveRecord::Observer
   def after_create(widget)
     if widget.page.nil?
       description = "#{widget.editor.name} added #{indefinite_articlerize(widget.content_type.split('_').first)} widget to the #{widget.canvas.name} community feed"
+			event = Event.create(:canvas_id => widget.canvas_id, :user_id => widget.creator_id, :loggable_id => widget.canvas.id, :loggable_type => 'Canvas', :description => description)
     else
       description = "#{widget.editor.name} added #{indefinite_articlerize(widget.content_type.split('_').first)} widget to the #{widget.page.title} page in the #{widget.canvas.name} community"
+			event = Event.create(:canvas_id => widget.canvas_id, :user_id => widget.creator_id, :loggable_id => widget.page.id, :loggable_type => 'Page', :description => description)
     end
 
-  	event = Event.create(:canvas_id => widget.canvas_id, :user_id => widget.creator_id, :loggable_id => widget.id, :loggable_type => 'Widget', :description => description)
+  	
   end
   
   def after_update(widget)
     if widget.position.nil? || widget.position_changed?
   	  if widget.page.nil?
         description = "#{widget.editor.name} updated #{indefinite_articlerize(widget.content_type.split('_').first)} widget in the #{widget.canvas.name} community feed"
+				event = Event.create(:canvas_id => widget.canvas_id, :user_id => widget.creator_id, :loggable_id => widget.canvas.id, :loggable_type => 'Canvas', :description => description)
       else
         description = "#{widget.editor.name} updated #{indefinite_articlerize(widget.content_type.split('_').first)} widget on the #{widget.page.title} page in the #{widget.canvas.name} community"
+				event = Event.create(:canvas_id => widget.canvas_id, :user_id => widget.creator_id, :loggable_id => widget.page.id, :loggable_type => 'Page', :description => description)
       end
-  	  event = Event.create(:canvas_id => widget.canvas_id, :user_id => widget.creator_id, :loggable_id => widget.id, :loggable_type => 'Widget', :description => description)
 	  end
   end
 end
